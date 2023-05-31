dc28d90c7926631f76d2080f9db2c077bf9337f9
Joshimello <joshualeanjw@gmail.com>
2023-05-31 23:59:51 +0800

    a

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { Canvas } from '@threlte/core'
+  import { World } from '@threlte/rapier'
+  import Scene from './Scene.svelte'
+</script>
+
+<main class="w-full h-100vh">
+  <Canvas>
+    <World>
+      <Scene/>
+    </World>
+  </Canvas>
+</main>
---
+++ src/Scene.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { T, useThrelte, useFrame } from '@threlte/core'
+  import { OrbitControls } from '@threlte/extras'
+  import { Color } from 'three/src/math/Color'
+
+  import Ground from './obj/Ground.svelte'
+  import Keyboard from './obj/Keyboard.svelte'
+  import Mouse from './obj/Mouse.svelte'
+
+  const { scene } = useThrelte()
+  scene.background = new Color('#22223B')
+</script>
+
+<T.HemisphereLight args={['#4A4E69', '#080820', 2]}/>
+<T.PointLight args={['#F2E9E4', 3, 15]} position={[8, 4, -2]} castShadow/>
+
+<T.PerspectiveCamera makeDefault position={[0, 4, 10]}>
+  <OrbitControls/>
+</T.PerspectiveCamera>
+
+<Keyboard/>
+<Mouse/>
+<Ground/>
+
+
+<!--
+#22223B #4A4E69 #9A8C98 #C9ADA7 #F2E9E4
+-->
